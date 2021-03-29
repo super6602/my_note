5f4d59646ebfd3e69414b0481737e1e92f43fbc7
super6602 <super6602.ece00g@g2.nctu.edu.tw>
2021-03-30 00:14:19 +0800

    [802.15.4] add 802.15.4 phy layer

--- sidebars.js
+++ sidebars.js
@@ -3,6 +3,6 @@ module.exports = {
     Linux: ['doc1', 'linux/device_tree', 'linux/linux_toolchain', 'linux/debugger'],
     arm:['arm/arm_coresight', 'arm/bare_metal_startup'],
     Zephyr: ['zephyr/zephyr', 'zephyr/west', 'zephyr/debugger', 'zephyr/ble_controller', 'zephyr/1_zephyr_startup', 'zephyr/2_zephyr_thread'],
-    IEEE_802_15_4: ['802.15.4/TCP-UDP', '802.15.4/6LoWPAN']
+    IEEE_802_15_4: ['802.15.4/TCP-UDP', '802.15.4/6LoWPAN', '802.15.4/802.15.4_PHY']
   },
 };
