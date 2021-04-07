49fa16c2391fd16d7c6338f89bbc6767953bf946
super6602 <super6602.ece00g@g2.nctu.edu.tw>
2021-04-07 17:32:35 +0800

    [freertos] add document

---
+++ docs/freertos/freertos-kernel.md
@@ -0,0 +1,10 @@
+---
+id: freertos-kernel
+title: freertos-kernel
+---
+
+## MISC
+
+[mastering freertos](https://freertoskernel.asicfans.com/)
+
+[freertos 參考手冊](https://freertos.asicfans.com/)
--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,9 @@ module.exports = {
   someSidebar: {
     Linux: ['doc1', 'linux/device_tree', 'linux/linux_toolchain', 'linux/debugger'],
     arm:['arm/arm_coresight', 'arm/bare_metal_startup' , 'arm/arm_isa', 'arm/arm_instruction'],
+    vm: ['vm/qemu'],
     Zephyr: ['zephyr/zephyr', 'zephyr/west', 'zephyr/debugger', 'zephyr/ble_controller', 'zephyr/1_zephyr_startup', 'zephyr/2_zephyr_thread'],
+    freertos: ['freertos/freertos-kernel'],
     IEEE_802_15_4: ['802.15.4/TCP-UDP', '802.15.4/6LoWPAN', '802.15.4/802.15.4_PHY', '802.15.4/802.15.4_MAC']
   },
 };
